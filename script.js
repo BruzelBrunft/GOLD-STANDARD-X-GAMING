5b8fa06ed4f5b25b63012ede4bd5fa0fe6e3d0d9
BruzelBrunft <72694015+BruzelBrunft@users.noreply.github.com>
2026-06-02 09:35:28 +0200

    Del all

--- script.js
+++
@@ -1,3 +0,0 @@
-function toggleMenu(){document.querySelector('.navlinks').classList.toggle('open')}
-var io=new IntersectionObserver(function(e){e.forEach(function(n){if(n.isIntersecting){n.target.classList.add('in');io.unobserve(n.target)}})},{threshold:.1});
-document.querySelectorAll('.reveal').forEach(function(el,i){el.style.transitionDelay=(Math.min(i%6,5)*.07)+'s';io.observe(el)});
